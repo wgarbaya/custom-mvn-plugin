34b3bc5b7e43fefa08df4f313293f7aeeed5f35d
Garbaya Walid <walid_g_91@hotmail.com>
2017-10-18 13:40:02 +0200

    plugin clean

--- customdeploy/src/main/java/fr/dilaw/mplugin/customdeploy/MyMojo.java
+++ customdeploy/src/main/java/fr/dilaw/mplugin/customdeploy/MyMojo.java
@@ -2,14 +2,13 @@ package fr.dilaw.mplugin.customdeploy;
 
 
 import java.io.File;
-
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Execute;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-
+import org.apache.maven.project.MavenProject;
 
 @Mojo(name = "pack")
 @Execute(goal="pack", phase =LifecyclePhase.INSTALL)
@@ -17,11 +16,15 @@ public class MyMojo extends AbstractMojo {
 	@Parameter(property = "msg",defaultValue = "from maven")
 	private String msg;
 
-	@Parameter( defaultValue = "${project.basedir}", readonly = true )
+	@Parameter( defaultValue = "${project}", readonly = true )
+	private MavenProject project;
+
+
+	@Parameter( defaultValue = "${project.basedir}" )
 	private File basedir;
 
 	public void execute() throws MojoExecutionException {
-		getLog().info("Hello " + msg + " " + "baseid : " +  basedir);
+		getLog().info("Hello " + msg + " " + "baseid : " + project.getBasedir());
 	}
 	public File getBasedir() {
 		return basedir;
